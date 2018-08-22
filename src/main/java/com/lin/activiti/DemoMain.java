09ec0038aaf8a4bed1e687dae6250101f4914eef
lkmc2 <lkmc2@163.com>
2018-08-23 00:15:42 +0800

    部署流程定义文件，并将bpmn文件后缀名改成xml

--- src/main/java/com/lin/activiti/DemoMain.java
+++ src/main/java/com/lin/activiti/DemoMain.java
@@ -2,6 +2,10 @@ package com.lin.activiti;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,6 +33,18 @@ public class DemoMain {
         LOGGER.info("流程引擎名称{},版本{}", name, version);
 
         // 2.部署流程定义文件
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        DeploymentBuilder deploymentBuilder = repositoryService.createDeployment();
+        deploymentBuilder.addClasspathResource("event.bpmn20.xml");
+        Deployment deploy = deploymentBuilder.deploy();
+
+        String deploymentId = deploy.getId();
+
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deploymentId)
+                .singleResult();
+
+        LOGGER.info("流程定义文件{}，流程ID{}", processDefinition.getName(), processDefinition.getId());
 
         // 3.启动流程引擎
 
