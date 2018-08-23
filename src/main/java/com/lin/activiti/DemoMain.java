538ac61376b6c948c98d37d5a4c9877186a3ce2a
lkmc2 <lkmc2@163.com>
2018-08-23 10:58:03 +0800

    完成一个流程的定义和启动

--- src/main/java/com/lin/activiti/DemoMain.java
+++ src/main/java/com/lin/activiti/DemoMain.java
@@ -1,14 +1,15 @@
 package com.lin.activiti;
 
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngineConfiguration;
-import org.activiti.engine.RepositoryService;
+import org.activiti.engine.*;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 
 
 /**
@@ -30,7 +31,7 @@ public class DemoMain {
         String name = processEngine.getName();
         String version = ProcessEngine.VERSION;
 
-        LOGGER.info("流程引擎名称{},版本{}", name, version);
+        LOGGER.info("流程引擎名称 [{}],版本 [{}]", name, version);
 
         // 2.部署流程定义文件
         RepositoryService repositoryService = processEngine.getRepositoryService();
@@ -44,11 +45,21 @@ public class DemoMain {
                 .deploymentId(deploymentId)
                 .singleResult();
 
-        LOGGER.info("流程定义文件{}，流程ID{}", processDefinition.getName(), processDefinition.getId());
+        LOGGER.info("流程定义文件 [{}]，流程ID [{}]", processDefinition.getName(), processDefinition.getId());
 
         // 3.启动流程引擎
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
+
+        LOGGER.info("启动流程 [{}]", processInstance.getProcessDefinitionKey());
 
         // 4.处理流程任务
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> list = taskService.createTaskQuery().list();
+        for (Task task : list) {
+            LOGGER.info("待处理任务 [{}]", task.getName());
+        }
+        LOGGER.info("待处理任务数量 [{}]", list.size());
 
         LOGGER.info("结束程序");
     }
