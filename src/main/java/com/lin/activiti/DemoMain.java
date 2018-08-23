87a563ff2848c3a7b3f276cf3e5caf592ad847e8
lkmc2 <lkmc2@163.com>
2018-08-23 11:19:09 +0800

    编写从控制台获取用户输入的逻辑

--- src/main/java/com/lin/activiti/DemoMain.java
+++ src/main/java/com/lin/activiti/DemoMain.java
@@ -1,6 +1,11 @@
 package com.lin.activiti;
 
+import com.google.common.collect.Maps;
 import org.activiti.engine.*;
+import org.activiti.engine.form.FormProperty;
+import org.activiti.engine.form.TaskFormData;
+import org.activiti.engine.impl.form.DateFormType;
+import org.activiti.engine.impl.form.StringFormType;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.engine.repository.ProcessDefinition;
@@ -9,7 +14,12 @@ import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 
 /**
@@ -21,19 +31,86 @@ import java.util.List;
 public class DemoMain {
     public static final Logger LOGGER = LoggerFactory.getLogger(DemoMain.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         LOGGER.info("启动程序");
 
-        // 1.创建流程引擎（创建基于默认数据库的流程引擎）
-        ProcessEngineConfiguration config =
-                ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration();
-        ProcessEngine processEngine = config.buildProcessEngine();
-        String name = processEngine.getName();
-        String version = ProcessEngine.VERSION;
-
-        LOGGER.info("流程引擎名称 [{}],版本 [{}]", name, version);
+        // 1.创建流程引擎
+        ProcessEngine processEngine = getProcessEngine();
 
         // 2.部署流程定义文件
+        ProcessDefinition processDefinition = getProcessDefinition(processEngine);
+
+        // 3.启动流程引擎
+        ProcessInstance processInstance = getProcessInstance(processEngine, processDefinition);
+
+        // 4.处理流程任务
+        Scanner scanner = new Scanner(System.in);
+
+        while (processInstance != null && !processInstance.isEnded()) {
+            TaskService taskService = processEngine.getTaskService();
+            List<Task> list = taskService.createTaskQuery().list();
+            LOGGER.info("待处理任务数量 [{}]", list.size());
+
+            for (Task task : list) {
+                LOGGER.info("待处理任务 [{}]", task.getName());
+                FormService formService = processEngine.getFormService();
+                TaskFormData taskFormData = formService.getTaskFormData(task.getId());
+                List<FormProperty> formProperties = taskFormData.getFormProperties();
+                HashMap<String, Object> variables = Maps.newHashMap();
+
+                for (FormProperty property : formProperties) {
+                    String line = null;
+
+                    if (StringFormType.class.isInstance(property.getType())) {
+                        LOGGER.info("请输入 {}？", property.getName());
+                        line = scanner.nextLine();
+                        variables.put(property.getId(), line);
+                    } else if (DateFormType.class.isInstance(property.getType())) {
+                        LOGGER.info("请输入 {}？ 格式（yyyy-MM-dd）", property.getName());
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                        line = scanner.nextLine();
+                        Date date = dateFormat.parse(line);
+                        variables.put(property.getId(), date);
+                    } else {
+                        LOGGER.info("类型不支持 {}", property.getType());
+                    }
+                    LOGGER.info("输入的内容是 [{}]", line);
+                }
+
+                // 提交任务（任务id，参数）
+                taskService.complete(task.getId(), variables);
+
+                // 重新查询获取任务实例
+                processInstance = processEngine.getRuntimeService()
+                        .createProcessInstanceQuery()
+                        .processInstanceId(processInstance.getId())
+                        .singleResult();
+            }
+        }
+
+        LOGGER.info("结束程序");
+    }
+
+    /**
+     * 启动流程引擎
+     * @param processEngine 流程引擎
+     * @param processDefinition 流程配置
+     * @return 流程实例
+     */
+    private static ProcessInstance getProcessInstance(ProcessEngine processEngine, ProcessDefinition processDefinition) {
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
+
+        LOGGER.info("启动流程 [{}]", processInstance.getProcessDefinitionKey());
+        return processInstance;
+    }
+
+    /**
+     * 部署流程定义文件
+     * @param processEngine 流程引擎
+     * @return 流程定义文件
+     */
+    private static ProcessDefinition getProcessDefinition(ProcessEngine processEngine) {
         RepositoryService repositoryService = processEngine.getRepositoryService();
         DeploymentBuilder deploymentBuilder = repositoryService.createDeployment();
         deploymentBuilder.addClasspathResource("event.bpmn20.xml");
@@ -46,22 +123,23 @@ public class DemoMain {
                 .singleResult();
 
         LOGGER.info("流程定义文件 [{}]，流程ID [{}]", processDefinition.getName(), processDefinition.getId());
+        return processDefinition;
+    }
 
-        // 3.启动流程引擎
-        RuntimeService runtimeService = processEngine.getRuntimeService();
-        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
+    /**
+     * 创建流程引擎
+     * @return 流程引擎
+     */
+    private static ProcessEngine getProcessEngine() {
+        // 基于默认数据库的流程引擎
+        ProcessEngineConfiguration config =
+                ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration();
+        ProcessEngine processEngine = config.buildProcessEngine();
+        String name = processEngine.getName();
+        String version = ProcessEngine.VERSION;
 
-        LOGGER.info("启动流程 [{}]", processInstance.getProcessDefinitionKey());
-
-        // 4.处理流程任务
-        TaskService taskService = processEngine.getTaskService();
-        List<Task> list = taskService.createTaskQuery().list();
-        for (Task task : list) {
-            LOGGER.info("待处理任务 [{}]", task.getName());
-        }
-        LOGGER.info("待处理任务数量 [{}]", list.size());
-
-        LOGGER.info("结束程序");
+        LOGGER.info("流程引擎名称 [{}],版本 [{}]", name, version);
+        return processEngine;
     }
 
 }
