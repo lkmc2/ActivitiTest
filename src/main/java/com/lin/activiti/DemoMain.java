20b559fe8011372800117a8572e0ab3631081bb1
lkmc2 <lkmc2@163.com>
2018-08-23 13:00:46 +0800

    简化代码逻辑

--- src/main/java/com/lin/activiti/DemoMain.java
+++ src/main/java/com/lin/activiti/DemoMain.java
@@ -44,6 +44,18 @@ public class DemoMain {
         ProcessInstance processInstance = getProcessInstance(processEngine, processDefinition);
 
         // 4.处理流程任务
+        processTask(processEngine, processInstance);
+
+        LOGGER.info("结束程序");
+    }
+
+    /**
+     * 处理流程任务
+     * @param processEngine 流程引擎
+     * @param processInstance 流程实例
+     * @throws ParseException 解析异常
+     */
+    private static void processTask(ProcessEngine processEngine, ProcessInstance processInstance) throws ParseException {
         Scanner scanner = new Scanner(System.in);
 
         while (processInstance != null && !processInstance.isEnded()) {
@@ -52,30 +64,7 @@ public class DemoMain {
             LOGGER.info("待处理任务数量 [{}]", list.size());
 
             for (Task task : list) {
-                LOGGER.info("待处理任务 [{}]", task.getName());
-                FormService formService = processEngine.getFormService();
-                TaskFormData taskFormData = formService.getTaskFormData(task.getId());
-                List<FormProperty> formProperties = taskFormData.getFormProperties();
-                HashMap<String, Object> variables = Maps.newHashMap();
-
-                for (FormProperty property : formProperties) {
-                    String line = null;
-
-                    if (StringFormType.class.isInstance(property.getType())) {
-                        LOGGER.info("请输入 {}？", property.getName());
-                        line = scanner.nextLine();
-                        variables.put(property.getId(), line);
-                    } else if (DateFormType.class.isInstance(property.getType())) {
-                        LOGGER.info("请输入 {}？ 格式（yyyy-MM-dd）", property.getName());
-                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                        line = scanner.nextLine();
-                        Date date = dateFormat.parse(line);
-                        variables.put(property.getId(), date);
-                    } else {
-                        LOGGER.info("类型不支持 {}", property.getType());
-                    }
-                    LOGGER.info("输入的内容是 [{}]", line);
-                }
+                HashMap<String, Object> variables = addConsoleParamsToMap(processEngine, scanner, task);
 
                 // 提交任务（任务id，参数）
                 taskService.complete(task.getId(), variables);
@@ -88,7 +77,43 @@ public class DemoMain {
             }
         }
 
-        LOGGER.info("结束程序");
+        scanner.close();
+    }
+
+    /**
+     * 将控制台参数添加到map中
+     * @param processEngine 流程引擎
+     * @param scanner 获取控制台输入的控件
+     * @param task 任务
+     * @return 带控制台参数的map
+     * @throws ParseException 解析异常
+     */
+    private static HashMap<String, Object> addConsoleParamsToMap(ProcessEngine processEngine, Scanner scanner, Task task) throws ParseException {
+        LOGGER.info("待处理任务 [{}]", task.getName());
+        FormService formService = processEngine.getFormService();
+        TaskFormData taskFormData = formService.getTaskFormData(task.getId());
+        List<FormProperty> formProperties = taskFormData.getFormProperties();
+        HashMap<String, Object> variables = Maps.newHashMap();
+
+        for (FormProperty property : formProperties) {
+            String line = null;
+
+            if (StringFormType.class.isInstance(property.getType())) {
+                LOGGER.info("请输入 {}？", property.getName());
+                line = scanner.nextLine();
+                variables.put(property.getId(), line);
+            } else if (DateFormType.class.isInstance(property.getType())) {
+                LOGGER.info("请输入 {}？ 格式（yyyy-MM-dd）", property.getName());
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                line = scanner.nextLine();
+                Date date = dateFormat.parse(line);
+                variables.put(property.getId(), date);
+            } else {
+                LOGGER.info("类型不支持 {}", property.getType());
+            }
+            LOGGER.info("输入的内容是 [{}]", line);
+        }
+        return variables;
     }
 
     /**
@@ -113,7 +138,7 @@ public class DemoMain {
     private static ProcessDefinition getProcessDefinition(ProcessEngine processEngine) {
         RepositoryService repositoryService = processEngine.getRepositoryService();
         DeploymentBuilder deploymentBuilder = repositoryService.createDeployment();
-        deploymentBuilder.addClasspathResource("event.bpmn20.xml");
+        deploymentBuilder.addClasspathResource("event.bpmn20.xml"); // 读取带流程过程的xml文件
         Deployment deploy = deploymentBuilder.deploy();
 
         String deploymentId = deploy.getId();
